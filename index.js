94b6fa8dd4e9b2f0fcaa172e1284ffe6ce37cadd
Dillon Long <dplong@crimson.ua.edu>
2020-07-29 14:22:47 -0700

    Finished working on phase 1 and 2 of the project

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+const csrf = require("csurf");
 
 const app = express();
+app.use(cookieParser());
+
+const csrfProtection = csrf({cookie: true});
+app.use(csrfProtection);
+
 const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render('index',{users});
 });
+
+app.get("/create", (req, res) => {
+  res.render('create', {csrfToken: req.csrfToken()});
+});
+
+// app.post("/create", (req, res) => {
+//   res.render('create', {csrfToken: req.csrfToken()});
+// });
 
 const users = [
   {
